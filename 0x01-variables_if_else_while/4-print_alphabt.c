b64cc2226efbb8fcd2a1bb589dee23a72f7f55f0
Omarrashad1 <Omarrashad1@github.com>
2023-02-17 06:55:31 -0800

    4-print_alphabet.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@ char c;
 c = 'a';
 while
 	(c <= 'z') {
-		if (( c != 'q' && c != 'e') && c <= 'z')
+		if ((c != 'q' && c != 'e') && c <= 'z')
 			putchar(c);
 		c++;
 	}
 putchar ('\n');
 return (0);
-}		
+}
