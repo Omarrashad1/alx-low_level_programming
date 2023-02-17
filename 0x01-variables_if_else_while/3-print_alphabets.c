7c9415d89af860f638b3c1691f17efeab13c8552
Omarrashad1 <Omarrashad1@github.com>
2023-02-17 06:43:54 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,21 @@
 
 int main(void)
 {
-	char c;
-	char d;
-	c = 'a';
-	d = 'A';
-	while
+
+char c;
+char d;
+c = 'a';
+d = 'A';
+while
 	(c <= 'z') {
 		putchar(c);
 		c++;
 	}
 while
-
-       (d <= 'Z') {
+(d <= 'Z') {
 	       putchar(d);
 	       d++;
-       }
+}
 putchar('\n');
 return (0);
 }
